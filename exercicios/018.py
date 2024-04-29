caa07aa4aaec2ac976d52a7ec229bf8a50fa21bd
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-04-29 19:00:13 -0300

    atualizei a pagina dos exercicios para HTML

---
+++ exercicios/018.py
@@ -0,0 +1,10 @@
+from math import sin, radians, cos, tan
+
+angulo = int(input('Digite o angulo que você deseja: '))
+seno = sin(radians(angulo))
+coseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
+
+print(f'O seno do angulo de {angulo}º é {seno:.2f}.\
+    \nO coseno do angulo de {angulo}º é {coseno:.2f}.\
+     \nA tangente do angulo de {angulo}º é {tangente:.2f} ')
