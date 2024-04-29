d7c362705409f26d36f43bbc7441c8d916ed80fa
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-04-29 13:59:37 -0300

    ex 017 concluido

---
+++ exercicios/017.py
@@ -0,0 +1,7 @@
+
+cateto_oposto = int(input('Cateto Oposto: '))
+cateto_adjacente = int(input('Cateto Adjacente: '))
+
+hipotenusa = (cateto_oposto**2 + cateto_adjacente**2) ** 0.5
+
+print (f'O valor da hipotenusa » {hipotenusa:.2f}')
