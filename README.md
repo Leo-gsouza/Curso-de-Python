b2e916c1f1caaf57012a77e93736a66d1ed940da
Leonardo Souza <143288413+Leo-gsouza@users.noreply.github.com>
2024-04-29 11:17:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Curso-de-Python
+ Exercicios do curso de python do Gustavo Guanabara organizado em uma página construida com HTML e CSS
---
+++ exercicios/016.py
@@ -0,0 +1,16 @@
+""" Obitendo a parte inteira usando Import math » trunc """
+
+import math
+
+numero = float(input('Digite um número: '))
+numero = math.trunc(numero)
+
+print(f'A porção inteira do número é {numero}')
+
+
+
+""" Obtendo a parte inteira usando INT """
+
+numero = float(input('Digite um número: '))
+
+print (f'A parte inteira do número é {int(numero)}')
